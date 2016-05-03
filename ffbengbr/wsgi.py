5353e07ef3aba71207ee07e28d4757d98cdb7331
Frederico Bittencourt <fredsan@Frederico-no-iMac.local>
2016-05-03 15:38:09 -0300

    Finished the part of Views in the tutorial

--- ffbengbr/wsgi.py
+++ ffbengbr/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+path='/Users/fredsan/Code/ffbengbr'
+if path not in sys.path:
+	sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ffbengbr.settings")
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
